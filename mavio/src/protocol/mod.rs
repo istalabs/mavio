a94971b9a7a9f2a156a248b4eca1142475e44b62
Mykhailo Ziatin <mikhail.zyatin@gmail.com>
2024-02-10 14:08:49 +0200

    Reformat `protocol` imports.

--- mavio/src/protocol/mod.rs
+++ mavio/src/protocol/mod.rs
@@ -2,9 +2,9 @@
 //!
 //! MAVLink protocol abstractions.
 //!
-//! We also re-export from [`mavspec::rust::spec`] crate to provide a full specification of MAVLink-related types.
+//! We also re-export from [`mavspec::rust::spec`] crate to provide a full specification of
+//! MAVLink-related types.
 
-// Re-export from `mavspec::rust::spec`
 #[doc(no_inline)]
 pub use mavspec::rust::spec::types::{CrcExtra, MessageId};
 #[doc(no_inline)]
@@ -13,24 +13,16 @@ pub use mavspec::rust::spec::{
     MessageSpec, Payload,
 };
 
-// Signature
-pub(super) mod signature;
-pub use signature::{MavTimestamp, SecretKey, Sign, Signature, SignatureConf};
-
-// Magic byte (STX)
-pub(crate) mod stx;
-pub use stx::MavSTX;
-
-// Header
-pub(super) mod header;
-pub use header::{Header, HeaderBytes};
-
-// MAVLink frame
 pub(super) mod frame;
-pub use frame::{Frame, FrameBuilder};
-
-// Common types
+pub(super) mod header;
+pub(super) mod signature;
+pub(crate) mod stx;
 pub(super) mod types;
+
+pub use frame::{Frame, FrameBuilder};
+pub use header::{Header, HeaderBytes};
+pub use signature::{MavTimestamp, SecretKey, Sign, Signature, SignatureConf};
+pub use stx::MavSTX;
 pub use types::{
     Checksum, CompatFlags, ComponentId, HeaderV1Bytes, HeaderV2Bytes, IncompatFlags, PayloadLength,
     Sequence, SignatureBytes, SignatureLinkId, SignatureTimestampBytes, SignatureValue, SystemId,
